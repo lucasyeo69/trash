8ee8c7134b993928c56577d56df5707c3e4989e9
lucasyeo <lucas.yeo69@gmail.com>
2021-06-25 09:59:02 +0800

    added scenes and mgr scenes stuf

--- sketch.js
+++ sketch.js
@@ -1,29 +1,31 @@
-var scene1 = true;
-var scene2 = false;
-var scene3 = false;
-var scene4 = false;
-var scene5 = false;
+var mgr;
+
 
 function setup() {
+  mgr = new SceneManager()
+  mgr.addScene(homescreen)
+  mgr.addScene(loginPage)
+  mgr.addScene(signInPage)
   createCanvas(windowHeight, windowWidth);
   textSize(10);
+
+  mgr.showNextScene()
 }
 
-function draw() {
-  background(0, 30, 40);
 
+function draw(){
+  mgr.draw()
+  }
 
-  if (scene1 == true) {
-    // draw scene 1
-    function draw(){
-  function homescreen(){
+function homescreen(){
+  this.draw = function(){
     fill(200)
     rect(50,50,256,348)
     fill(90)
     rect(50,50,256,30)
     fill(0)
     let s = 'Saving the planet one litter at a time';
-    text(s, 70, 60, 250, 30);
+    texts, 70, 60, 250, 30);
     let a = 'Sign up'
     text(a,70,100,290,40)
     let b = "Login"
@@ -42,12 +44,10 @@ function draw() {
     fill(0)
     let f = 'Sign in'
     text(f,167,307,190,40)
+   }
  }
- homescreen()
-}
-draw()
-
-  } else if (scene2 == true) {
+function loginPage(){
+  this.draw = function(){
     fill(100,100,100);
     rect(50,50,256,348)
     let g = 'Username:'
@@ -58,7 +58,12 @@ draw()
     text(h,70,170,100,40)
     let m = "Login"
     text(m,170,350,100,40)
-  } else if(scene3 == true){
+  }
+
+}
+
+function signInPage(){
+  this.draw = function(Signinpage){
     fill(100,100,100);
     rect(50,50,256,348)
     let i = 'Email:'
@@ -73,20 +78,4 @@ draw()
     text(l,130,350,100,40)
   }
 
-}
-
-
-
-function mousePressed() {
-  if (scene1 == true) {
-    if (mouseX < width / 2) {
-      //do something
-      scene2 = true;
-    } else if (mouseX > width / 2) {
-      //do something else
-      scene3 = true;
-    }
-    // turn scene 1 off
-    scene1 = false;
-  }
 }
